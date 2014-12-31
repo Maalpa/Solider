375896b6b012dc23812b11a462e859c719cbdd12
Pawel Kepa <Pawel.Kepa01@external.t-mobie.pl>
2014-12-31 13:23:45 +0100

    Add collision to Tank

--- Web/core/target/classes/com/solider/war/core/GameStatics.java
+++
@@ -1,7 +0,0 @@
-package com.solider.war.core;
-
-public class GameStatics {
-	public static String RED_SOLIDER = "sprites/solider_20px_red.png";
-	public static String GREEN_SOLIDER = "sprites/solider_20px.png";
-//	public static String GREEN_SOLIDER = "sprites/solider.png";
-}
